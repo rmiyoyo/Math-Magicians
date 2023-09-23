7dd90580a44d107b61f4c336e386b5d8df6b57bc
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-23 23:35:14 +0300

    Add calculator functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,235 @@
-const MathApp = () => {
-  const Input = () => (<div className="enter-widget"><input className="input" type="number" placeholder="0" /></div>);
-  const Buttons = () => (
-    <>
-      <div className="Maths-Widgets"><button className="AC-btn" type="button">AC</button></div>
-      <div className="arithmetic-device-container"><button className="arithmetic-device" type="button">+/-</button></div>
-      <div className="rate-widget"><button className="rate-button" type="button">%</button></div>
-      <div className="manipulate-button"><button className="quotient" type="button">÷</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">7</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">8</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">9</button></div>
-      <div className="manipulate-button"><button className="multiplication" type="button">x</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">4</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">5</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">6</button></div>
-      <div className="manipulate-button"><button className="minus-class" type="button">-</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">1</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">2</button></div>
-      <div className="figure-button"><button className="figure-button" type="button">3</button></div>
-      <div className="manipulate-button"><button className="plus-class" type="button">+</button></div>
-      <div className="figure-button zero-class"><button className="zero-class" type="button">0</button></div>
-      <div className="period-widget"><button className="dot" type="button">.</button></div>
-      <div className="manipulate-button"><button className="equal" type="button">=</button></div>
-    </>
-  );
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
+const Calculator = () => {
+  const [info, initInfo] = useState({ total: '', next: '', operation: '' });
   return (
     <div className="wrapper">
-      <Input />
-      <Buttons />
+      <Entry data={info} />
+      <AllClearButton name="AC" data={info} initInfo={initInfo} />
+      <AddRemoveButton name="+/-" data={info} initInfo={initInfo} />
+      <RemainderButton name="%" data={info} initInfo={initInfo} />
+      <SplitButton name="÷" data={info} initInfo={initInfo} />
+      <SevenButton name="7" data={info} initInfo={initInfo} />
+      <EightButton name="8" data={info} initInfo={initInfo} />
+      <NineButton name="9" data={info} initInfo={initInfo} />
+      <ProductButton name="x" data={info} initInfo={initInfo} />
+      <FourButton name="4" data={info} initInfo={initInfo} />
+      <FiveButton name="5" data={info} initInfo={initInfo} />
+      <SixButton name="6" data={info} initInfo={initInfo} />
+      <RemoveButton name="-" data={info} initInfo={initInfo} />
+      <OneButton name="1" data={info} initInfo={initInfo} />
+      <TwoButton name="2" data={info} initInfo={initInfo} />
+      <ThreeButton name="3" data={info} initInfo={initInfo} />
+      <PlusButton name="+" data={info} initInfo={initInfo} />
+      <NonumButton name="0" data={info} initInfo={initInfo} />
+      <PeriodButton name="." data={info} initInfo={initInfo} />
+      <SameButton name="=" data={info} initInfo={initInfo} />
     </div>
   );
 };
-export default MathApp;
+const Entry = ({ data }) => (<div className="input-box"><input className="input" type="number" placeholder="0" value={data.next || data.total || ''} readOnly /></div>);
+
+const OneButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>1</button>);
+const TwoButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>2</button>);
+const ThreeButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>3</button>);
+const FourButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>4</button>);
+const FiveButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>5</button>);
+const SixButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>6</button>);
+const SevenButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>7</button>);
+const EightButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>8</button>);
+const NineButton = ({ name, data, initInfo }) => (<button className="button number-button" type="button" onClick={() => initInfo(calculate(data, name))}>9</button>);
+const NonumButton = ({ name, data, initInfo }) => (<button className="button nonum" type="button" onClick={() => initInfo(calculate(data, name))}>0</button>);
+const PeriodButton = ({ name, data, initInfo }) => (<button className="button period" type="button" onClick={() => initInfo(calculate(data, name))}>.</button>);
+
+const PlusButton = ({ name, data, initInfo }) => (<button className="button plus" type="button" onClick={() => initInfo(calculate(data, name))}>+</button>);
+const RemoveButton = ({ name, data, initInfo }) => (<button className="button remove" type="button" onClick={() => initInfo(calculate(data, name))}>-</button>);
+const ProductButton = ({ name, data, initInfo }) => (<button className="button product" type="button" onClick={() => initInfo(calculate(data, name))}>x</button>);
+const SplitButton = ({ name, data, initInfo }) => (<button className="button split" type="button" onClick={() => initInfo(calculate(data, name))}>÷</button>);
+
+const RemainderButton = ({ name, data, initInfo }) => (<button className="button remainder-button" type="button" onClick={() => initInfo(calculate(data, name))}>%</button>);
+const AddRemoveButton = ({ name, data, initInfo }) => (<button className="button add-remove-button" type="button" onClick={() => initInfo(calculate(data, name))}>+/-</button>);
+const AllClearButton = ({ name, data, initInfo }) => (<button className="button all-clear" type="button" onClick={() => initInfo(calculate(data, name))}>AC</button>);
+const SameButton = ({ name, data, initInfo }) => (<button className="button same" type="button" onClick={() => initInfo(calculate(data, name))}>=</button>);
+
+Entry.propTypes = {
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+};
+AllClearButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+AddRemoveButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+RemainderButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+SplitButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+SevenButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+EightButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+NineButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+ProductButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+FourButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+FiveButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+SixButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+RemoveButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+OneButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+TwoButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+ThreeButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+PlusButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+NonumButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+PeriodButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+SameButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    total: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    next: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    operation: PropTypes.string,
+  }).isRequired,
+  initInfo: PropTypes.func.isRequired,
+};
+
+export default Calculator;
